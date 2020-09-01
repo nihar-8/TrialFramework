ad196de4e303db140bb283c672a392dcb8655609
Nihar Choudhary <nihar@technoexponent.com>
2020-09-01 18:11:37 +0530

    initial

---
+++ TrialFramework/TrialFramework/TrialFramework.h
@@ -0,0 +1,19 @@
+//
+//  TrialFramework.h
+//  TrialFramework
+//
+//  Created by Deepak on 01/09/20.
+//  Copyright © 2020 Nihar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrialFramework.
+FOUNDATION_EXPORT double TrialFrameworkVersionNumber;
+
+//! Project version string for TrialFramework.
+FOUNDATION_EXPORT const unsigned char TrialFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrialFramework/PublicHeader.h>
+
+
